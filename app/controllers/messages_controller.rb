fafa8627bae1278f0e55e880f91abb6704c00845
LieuFukunaga <heartarium01208.2@gmail.com>
2020-03-15 11:21:41 +0900

    画像アップローダーの準備をしました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: current_group.id)
+  end
 end
