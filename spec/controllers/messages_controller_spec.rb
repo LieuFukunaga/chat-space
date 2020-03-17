c097db0dadd336ccd2a696fc1ad4abedb8c00c12
LieuFukunaga <heartarium01208.2@gmail.com>
2020-03-17 20:30:07 +0900

    コントローラのテスト：createアクション：ログインしていて保存できるまで

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+describe MessagesController do
+
+# letを利用してテスト中使用するインスタンスを定義
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'renders index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context 'log in' do
+
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+
+      end
+
+      context 'can not save' do
+      end
+
+    end
+
+    context 'not log in' do
+      before do
+        get :create, params: params
+      end
+    end
+
+  end
+end
