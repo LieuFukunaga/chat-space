cf7a0a46eb9c05f652b00352d7013a2175f3b6f6
LieuFukunaga <heartarium01208.2@gmail.com>
2020-03-20 10:24:38 +0900

    6. 返ってきたJSONをdoneメソッドで受取り、HTMLを作成する

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,48 @@
 $(function() {
+  function buildHTML(message){
+    // jbuilderから返されたデータにimageが在る場合（＝画像を投稿した場合）
+    if( message.image ) {
+      let html =
+      // ① 該当箇所をCopy element
+      // ② 階層構造を整える
+      // ③ <%= %>を書き換え
+        `<div class="message">
+          <div class="upper-message">
+            <div class="user-name">
+              ${message.user_name}
+            </div>
+            <div class="time-stamp">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.body}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      `<div class="message">
+          <div class="upper-message">
+            <div class="user-name">
+              ${message.user_name}
+            </div>
+            <div class="time-stamp">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.body}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +54,9 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
     })
   })
 });
