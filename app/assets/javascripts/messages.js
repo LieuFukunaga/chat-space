a38aa53eed957d4c2c2eb3816a4358d3624c670f
LieuFukunaga <heartarium01208.2@gmail.com>
2020-03-21 16:58:36 +0900

    「取得した最新のメッセージをブラウザのメッセージ一覧に追加しよう」まで

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,43 +1,49 @@
 $(function() {
-  function buildHTML(message){
-    if( message.image ) {
-      let html =
-        `<div class="message">
-          <div class="upper-message">
-            <div class="user-name">
-              ${message.user_name}
-            </div>
-            <div class="time-stamp">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.body}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      let html=
-      `<div class="message">
-          <div class="upper-message">
-            <div class="user-name">
-              ${message.user_name}
-            </div>
-            <div class="time-stamp">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.body}
-            </p>
-          </div>
-        </div>`
-      return html;
+  let last_message_id = $('.message:last').data("message-id");
+  function buildHTML(data){
+    let upperMessage =
+    `<div class="upper-message">` +
+      `<div class="user-name">` +
+        message.user_name +
+      `</div>` +
+      `<div class="date">` +
+        message.created_at +
+      `</div>` +
+    `</div>`
+
+    // formDataにbodyとimageがある場合（＝文字と写真が投稿された場合）
+    if( message.body && message.image ) {
+      let html = `<div class="message" data-message-id=` + message.id + `>` +
+        upperMessage +
+        `<div class="lower-message">` +
+          `<p class="lower-message__body">` +
+            message.body +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+
+    // 文字だけの投稿の場合
+    } else if (message.body) {
+      let html = `<div class="message" data-message-id=` + message.id + `>` +
+        upperMessage +
+        `<div class="lower-message">` +
+          `<p class="lower-message__body">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+
+    // 画像だけの投稿の場合
+    } else if (message.image) {
+      let html = `<div class="message" data-message-id=` + message.id + `>` +
+        upperMessage
+        `<div class="lower-message">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
     };
+    return html;
   }
 
   $('#new_message').on('submit', function(e){
@@ -65,4 +71,24 @@ $(function() {
       $(".submit-btn").prop('disabled', false);
     })
   })
+
+  let reloadMessages = function () {
+    let last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messagess",
+      type: "get",
+      dataType: "json",
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      let insertHTML = '';
+      $.each(messages, function(i, message){
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+    })
+    .fail(function(){
+      alert("error");
+    });
+  };
 });
